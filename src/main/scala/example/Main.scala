efb24c153f7243ac2cf06c9bdd4239d5dc39ba8a
Jul Gvoz <julgvoz@gmail.com>
2020-02-19 13:59:41 +0200

    Currencies (Chapter 20.10)

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,25 +1,6 @@
 package example
 
-import queue._
-import types._
+import currencies.CurrencyZone
 
 object ExampleShow extends App {
-    class Inty(val x: Int)
-    class BetterInty(override val x: Int) extends Inty(x) {
-        val double = x + x
-    }
-
-    class Stringy(val s: String)
-    class BetterStringy(override val s: String) extends Stringy(s) {
-        val double = s + s
-    }
-
-    def intyToStringy(x: Inty): Stringy =
-        new BetterStringy(x.x.toString)
-    
-    val btrStr: Stringy = intyToStringy(new BetterInty(5))
-
-    val str: String = btrStr.s
-
-    println(str + " eh")
 } 
