22ac5e69f13be5a24134629981d030f44fa0cc56
JulGvoz <julgvoz@tutanota.com>
2020-06-14 15:04:02 +0300

    Implicit parameter showoff

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,10 +1,27 @@
 package example
-
-import implicits.Implicits
-import rational.Rational
 object ExampleShow extends App {
-  val a: Int = 15
-  val r: Rational = new Rational(3, 2)
+  case class PreferredPrompt(val preference: String)
+  case class PreferredDrink(val preference: String)
 
-  println(a + r)
+  object Greeter {
+    def greet(name: String)(implicit prompt: PreferredPrompt, drink: PreferredDrink): Unit = {
+      println("Welcome "+ name +". The system is ready.")
+      println("But while you wrok, why not enjoy a cup of "+ drink.preference +"?")
+      println(prompt.preference)
+    }
+  }
+
+  object JoesPrefs {
+    implicit val prompt = new PreferredPrompt("Yes, master> ")
+    implicit val drink = new PreferredDrink("tea")
+  }
+
+  val bobsPrompt = new PreferredPrompt("relax> ")
+  val bobsDrink = new PreferredDrink("coffee")
+
+  Greeter.greet("Bob")(bobsPrompt, bobsDrink)
+
+  import JoesPrefs._
+
+  Greeter.greet("Joe")
 } 
