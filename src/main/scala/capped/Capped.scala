e6c6bf4f813a5d87ad28f4afac46c8429578058c
JulGvoz <julgvoz@tutanota.com>
2020-06-22 12:35:09 +0300

    Finish Capped implementation

--- src/main/scala/capped/Capped.scala
+++ src/main/scala/capped/Capped.scala
@@ -2,41 +2,76 @@ package capped
 
 import scala.collection._
 
-class Capped1[A] private (val capacity: Int, val length: Int,
+class Capped[A] private (val capacity: Int, val length: Int,
   offset: Int, elems: Array[Any])
-  extends immutable.Iterable[A] { 
+  extends immutable.Iterable[A] 
+  with IterableOps[A, Capped, Capped[A]] 
+  with StrictOptimizedIterableOps[A, Capped, Capped[A]] { 
 
-    self => def this (capacity: Int) = this(capacity, length = 0, offset = 0, elems = Array.ofDim(capacity))
+  self => def this (capacity: Int) = this(capacity, length = 0, 
+    offset = 0, elems = Array.ofDim(capacity))
 
-    def appended[B >: A](elem: B): Capped1[B] = {
-      val newElems = Array.ofDim[Any](capacity)
-      Array.copy(elems, 0, newElems, 0, capacity)
-      val (newOffset, newLength) = {
-        if (length == capacity) {
-          newElems(offset) = elem
-          ((offset + 1) % capacity, length)
-        } else {
-          newElems(length) = elem
-          (offset, length + 1)
-        }
-      }
-      new Capped1[B](capacity, newLength, newOffset, newElems)
-    }
-
-    @inline def :+ [B >: A](elem: B): Capped1[B] = appended(elem)
-
-    def apply(i: Int): A = 
-      elems((i + offset) % capacity).asInstanceOf[A]
-    
-    def iterator: Iterator[A] = new AbstractIterator[A] {
-      private var current = 0
-      def hasNext = current < self.length
-      def next(): A = {
-        val elem = self(current)
-        current += 1
-        elem
+  def appended[B >: A](elem: B): Capped[B] = {
+    val newElems = Array.ofDim[Any](capacity)
+    Array.copy(elems, 0, newElems, 0, capacity)
+    val (newOffset, newLength) = {
+      if (length == capacity) {
+        newElems(offset) = elem
+        ((offset + 1) % capacity, length)
+      } else {
+        newElems(length) = elem
+        (offset, length + 1)
       }
     }
-
-    override def className = "Capped1"
+    new Capped[B](capacity, newLength, newOffset, newElems)
   }
+
+  @inline def :+ [B >: A](elem: B): Capped[B] = appended(elem)
+
+  def apply(i: Int): A = 
+    elems((i + offset) % capacity).asInstanceOf[A]
+
+  override def className = "Capped"
+
+  override val iterableFactory: IterableFactory[Capped] = 
+    new CappedFactory(capacity)
+  
+  override def fromSpecific(
+    coll: IterableOnce[A]): Capped[A] = 
+    iterableFactory.from(coll)
+
+  override protected def newSpecificBuilder:
+    mutable.Builder[A, Capped[A]] = 
+    iterableFactory.newBuilder
+  
+  override def empty: Capped[A] = iterableFactory.empty
+
+  def iterator: Iterator[A] = view.iterator
+
+  override def view: IndexedSeqView[A] = new IndexedSeqView[A] {
+    def length: Int = self.length
+    def apply(i: Int): A = self(i)
+  }
+
+  override def knownSize: Int = length
+}
+
+class CappedFactory(capacity: Int) extends
+  IterableFactory[Capped] {
+
+  def from[A](source: IterableOnce[A]): Capped[A] =
+    source match {
+      case capped: Capped[A] if capped.capacity == capacity => capped
+      case _ => (newBuilder[A] ++= source).result()
+    }  
+  
+  def empty[A]: Capped[A] = new Capped[A](capacity)
+
+  def newBuilder[A]: mutable.Builder[A, Capped[A]] = 
+    new mutable.ImmutableBuilder[A, Capped[A]](empty) {
+      def addOne(elem: A): this.type = {
+        elems = elems :+ elem
+        this
+      }
+    }
+}
