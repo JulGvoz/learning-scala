a52f5579321fee3c2495edcb3258ee056032fbd6
JulGvoz <julgvoz@tutanota.com>
2020-06-23 13:28:04 +0300

    Finish RNA

--- src/main/scala/rna/RNA.scala
+++ src/main/scala/rna/RNA.scala
@@ -4,12 +4,17 @@ import rna._
 import collection.mutable
 import collection.immutable.{IndexedSeq, IndexedSeqOps}
 import collection.View
+import scala.collection.StrictOptimizedSeqOps
+import scala.collection.SpecificIterableFactory
+import scala.collection.AbstractIterable
+import scala.collection.AbstractIterator
 
-final class RNA2 private (val groups: Array[Int],
+final class RNA private (val groups: Array[Int],
   val length: Int) extends IndexedSeq[Base]
-  with IndexedSeqOps[Base, IndexedSeq, RNA2] {
+  with IndexedSeqOps[Base, IndexedSeq, RNA] 
+  with StrictOptimizedSeqOps[Base, IndexedSeq, RNA] { rna =>
   
-  import rna.RNA2._
+  import RNA._
 
   def apply(idx: Int): Base = {
     if (idx < 0 || length <= idx)
@@ -18,42 +23,54 @@ final class RNA2 private (val groups: Array[Int],
   }
 
   override protected def fromSpecific(
-    source: IterableOnce[Base]): RNA2 = 
-    fromSeq(source.iterator.toSeq)
+    source: IterableOnce[Base]): RNA = 
+    RNA.fromSpecific(source)
   
-  override protected def newSpecificBuilder: mutable.Builder[Base, RNA2] = 
-    iterableFactory.newBuilder[Base].mapResult(fromSeq)
+  override protected def newSpecificBuilder: mutable.Builder[Base, RNA] = 
+    RNA.newBuilder
 
-  override def empty: RNA2 = fromSeq(Seq.empty)
+  override def empty: RNA = RNA.empty
 
-  override def className: String = "RNA2"
+  override def className: String = "RNA"
 
-  def appended(base: Base): RNA2 = 
-  fromSpecific(new View.Appended(this, base))
+  def appended(base: Base): RNA = 
+    (newSpecificBuilder ++= this += base).result()
 
-  def appendedAll(suffix: IterableOnce[Base]): RNA2 = 
+  def appendedAll(suffix: IterableOnce[Base]): RNA = 
+    strictOptimizedConcat(suffix, newSpecificBuilder)
+  
+  def concat(suffix: IterableOnce[Base]): RNA =
+    strictOptimizedConcat(suffix, newSpecificBuilder)
+  
+  def flatMap(f: Base => IterableOnce[Base]): RNA =
+    strictOptimizedFlatMap(newSpecificBuilder, f)
+  
+  def map(f: Base => Base): RNA =
+    strictOptimizedMap(newSpecificBuilder, f)
+  
+  def prepended(elem: Base): RNA = 
+    (newSpecificBuilder += elem ++= this).result()
+  
+  def prependedAll(prefix: IterableOnce[Base]): RNA =
+    (newSpecificBuilder ++= prefix ++= this).result()
+  
+  @inline final def ++ (suffix: IterableOnce[Base]): RNA =
     concat(suffix)
   
-  def concat(suffix: IterableOnce[Base]): RNA2 =
-    fromSpecific(this.iterator ++ suffix.iterator)
-  
-  def flatMap(f: Base => IterableOnce[Base]): RNA2 =
-    fromSpecific(new View.FlatMap(this, f))
-  
-  def map(f: Base => Base): RNA2 =
-    fromSpecific(new View.Map(this, f))
-  
-  def prepended(elem: Base): RNA2 = 
-    fromSpecific(new View.Prepended(elem, this))
-  
-  def prependedAll(prefix: IterableOnce[Base]): RNA2 =
-    fromSpecific(prefix.iterator ++ this.iterator)
-  
-  @inline final def ++ (suffix: IterableOnce[Base]): RNA2 =
-    concat(suffix)
+  override def iterator: Iterator[Base] =
+    new AbstractIterator[Base] {
+      private var i = 0
+      private var b = 0
+      def hasNext: Boolean = i < rna.length
+      def next(): Base = {
+        b = if (i % N == 0) groups(i / N) else b >>> S
+        i += 1
+        Base.fromInt(b & M)
+      }
+    }
 }
 
-object RNA2 {
+object RNA extends SpecificIterableFactory[Base, RNA] {
   // Number of bits necessary to represent group
   private val S = 2
   // Number of groups that fit in an Int
@@ -62,12 +79,20 @@ object RNA2 {
   private val M = (1 << S) - 1
 
 
-  def fromSeq(buf: collection.Seq[Base]): RNA2 = {
+  def fromSeq(buf: collection.Seq[Base]): RNA = {
     val groups = new Array[Int]((buf.length + N - 1) / N)
     for (i <- 0 until buf.length)
       groups(i / N) |= Base.toInt(buf(i)) << (i % N * S)
-    new RNA2(groups, buf.length)
+    new RNA(groups, buf.length)
   }
 
-  def apply(bases: Base*) = fromSeq(bases)
+  def fromSpecific(it: IterableOnce[Base]): RNA = it match {
+    case seq: collection.Seq[Base] => fromSeq(seq)
+    case _ => fromSeq(mutable.ArrayBuffer.from(it))
+  }
+
+  def empty: RNA = fromSeq(Seq.empty)
+
+  def newBuilder: mutable.Builder[Base,RNA] = 
+    mutable.ArrayBuffer.newBuilder[Base].mapResult(fromSeq)
 }
