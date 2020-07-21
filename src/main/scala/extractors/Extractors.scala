e97a97effd629774a69cc98df01260fb385a1f17
JulGvoz <julgvoz@tutanota.com>
2020-07-21 14:32:21 +0300

    a few more examples

--- src/main/scala/extractors/Extractors.scala
+++ src/main/scala/extractors/Extractors.scala
@@ -20,3 +20,29 @@ object Twice extends (String => String) {
     if (half == s.substring(length)) Some(half) else None
   }
 }
+
+object UpperCase {
+  def unapply(s: String): Boolean = s.toUpperCase == s
+}
+
+object Example {
+  def userTwiceUpper(s: String) = s match {
+    case Email(Twice(x @ UpperCase()), domain) =>
+      "match: " + x + " in domain " + domain
+    case _ => 
+      "no match"
+  }
+
+  def isTomInDotCom(s: String): Boolean = s match {
+    case Email("tom", Domain("com", _*)) => true
+    case _ => false
+  }
+}
+
+object Domain {
+  def apply(parts: String*): String = 
+    parts.reverse.mkString(".")
+  
+  def unapplySeq(whole: String): Option[Seq[String]] = 
+    Some(whole.split("\\.").reverse.toSeq)
+}
