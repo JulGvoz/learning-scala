6661d43c015c23af5553f8749dac355d0035ec89
JulGvoz <julgvoz@tutanota.com>
2020-08-01 18:11:31 +0300

    xml pattern matching

--- src/main/scala/xml/XML.scala
+++ src/main/scala/xml/XML.scala
@@ -36,13 +36,55 @@ object Example {
   val jname = joe \ "@name"
 
   val therm = new CCTherm {
-           val description = "hot dog #5"
-           val yearMade = 1952
-           val dateObtained = "March 14, 2006"
-           val bookPrice = 2199
-           val purchasePrice = 500
-           val condition = 9
-         }
+    val description = "hot dog #5"
+    val yearMade = 1952
+    val dateObtained = "March 14, 2006"
+    val bookPrice = 2199
+    val purchasePrice = 500
+    val condition = 9
+  }
+  
+  def proc(node: scala.xml.Node): String = 
+    node match {
+      case <a>{contents}</a> => "It's an a: " + contents
+      case <b>{contents}</b> => "It's a b: " + contents
+      case _ => "It's something else"
+    }
+  
+  def proc2(node: scala.xml.Node): String = 
+    node match {
+      case <a>{contents @ _*}</a> => "It's an a:" + contents
+      case <b>{contents @ _*}</b> => "It's a b:" + contents
+      case _ => "It's something else"
+    }
+  
+  val catalog =
+    <catalog>
+      <cctherm>
+        <description>hot dog #5</description>
+        <yearMade>1952</yearMade>
+        <dateObtained>March 14, 2006</dateObtained>
+        <bookPrice>2199</bookPrice>
+        <purchasePrice>500</purchasePrice>
+        <condition>9</condition>
+      </cctherm>
+      <cctherm>
+        <description>Sprite Boy</description>
+        <yearMade>1964</yearMade>
+        <dateObtained>April 28, 2003</dateObtained>
+        <bookPrice>1695</bookPrice>
+        <purchasePrice>595</purchasePrice>
+        <condition>5</condition>
+      </cctherm>
+    </catalog>
+
+  def run(): Unit = {
+    catalog match {
+      case <catalog>{therms @ _*}</catalog> =>
+        for (therm @ <cctherm>{_*}</cctherm> <- therms)
+          println("processing: " + (therm \ "description").text)
+    }
+  }
 }
 
 abstract class CCTherm {
