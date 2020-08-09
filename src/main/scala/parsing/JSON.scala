c05a25825bf26973592290b41867659a198ec960
JulGvoz <julgvoz@tutanota.com>
2020-08-09 20:29:37 +0300

    Giving correct types to JSON parser output

--- src/main/scala/parsing/JSON.scala
+++ src/main/scala/parsing/JSON.scala
@@ -4,10 +4,15 @@ import scala.util.parsing.combinator._
 
 class JSON extends JavaTokenParsers {
   def value: Parser[Any] = obj | arr | stringLiteral |
-    floatingPointNumber | "null" | "true" | "false"
-  def obj: Parser[Any] = "{"~repsep(member, ",")~"}"
-  def arr: Parser[Any] = "["~repsep(value, ",")~"]"
-  def member: Parser[Any] = stringLiteral~":"~value
+    floatingPointNumber ^^ (_.toDouble) | "null" | "true" ^^ (_ => true) | "false" ^^ (_ => false)
+  
+  def obj: Parser[Map[String, Any]] = "{"~> repsep(member, ",") <~"}" ^^ (Map() ++ _)
+
+  def arr: Parser[List[Any]] = "["~> repsep(value, ",") <~"]"
+
+  def member: Parser[(String, Any)] = stringLiteral~":"~value ^^ {
+    case name~":"~value => (name, value)
+  }
 }
 
 /**
